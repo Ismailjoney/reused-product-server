f51f93af38869b0f4811110c9b1518d571f95600
Ismailjoney <mohammadismailjoney@gmail.com>
2023-03-03 13:59:31 +0600

    make wt token and sent client side

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require(`express`)
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,8 +12,8 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://resaleproducts:BDiMKzoLVzeXzz5N@cluster0.i8hxp3j.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i8hxp3j.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -22,6 +24,7 @@ async function run() {
     try {
         const categoryCollection = client.db('resaleproducts').collection('categories')
         const categoryItemsCollection = client.db('resaleproducts').collection('categoriesItems')
+        const  usersCollection = client.db('resaleproducts').collection('users')
 
 
 
@@ -39,6 +42,28 @@ async function run() {
             const categorieitems = await categoryItemsCollection.find(query).toArray();
             res.send(categorieitems)
         })
+
+        //get jwt by email
+        app.get('/jwt', async(req,res) => {
+            const email = req.query.email;
+            console.log(email)
+            const query = {email : email}
+            const user = await usersCollection.findOne(query);
+
+            if(user){
+                const token = jwt.sign({email}, process.env.ACESS_TOKEN, { expiresIn :'7d'})
+                return res.send ({accessToken : token})
+            }
+            res.status(403).send({ accessToken : ''})
+        })
+
+        //users information save in data base
+        app.post('/users', async( req, res ) =>{
+            const users = req.body;
+            const resualt = await usersCollection.insertOne(users)
+            res.send(resualt)
+        })
+
 
 
 
